b5fb24a048c4f023678c79714224e43aaa6a547e
khairi <khairibouzid95@gmail.com>
2023-09-09 11:39:50 +0100

    first commit

--- client/index.js
+++ client/index.js
@@ -5,13 +5,34 @@ const MerkleTree = require('../utils/MerkleTree');
 const serverUrl = 'http://localhost:1225';
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+  // Create a Merkle tree for the nice list
+  const merkleTree = new MerkleTree(niceList);
 
-  const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
-  });
+  const Name = 'khairi';
 
-  console.log({ gift });
+  // Find the index of your name in the nice list
+  const index = niceList.findIndex(name => name === Name);
+
+  if (index === -1) {
+    console.log(`Sorry, ${Name} is not on the nice list.`);
+    return;
+  }
+
+  // Get the Merkle proof for your name
+  const proof = merkleTree.getProof(index);
+  
+
+  try {
+    // Make a request to the server to claim your gift
+    const { data: gift } = await axios.post(`${serverUrl}/gift`, {
+      name: Name,
+      proof: proof,
+    });
+
+    console.log({ gift });
+  } catch (error) {
+    console.error('Error claiming the gift:', error.message);
+  }
 }
 
 main();
